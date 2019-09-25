242a720d568d89e1fc948d408be6ef67bda5c90b
Xu Bin <xubin727@163.com>
2019-09-25 14:40:29 +0800

    x

---
+++ Xubin/Wxpayapi/WxPay.Exception.php
@@ -0,0 +1,17 @@
+<?php
+namespace Xubin\Wxpayapi;
+
+use Exception;
+
+/**
+ *
+ * 微信支付API异常类
+ * @author widyhu
+ *
+ */
+class WxPayException extends Exception {
+	public function errorMessage()
+	{
+		return $this->getMessage();
+	}
+}
